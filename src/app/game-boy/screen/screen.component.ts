6df274f90a89468e68545acf4bbf9f3cb0927579
Tiago Campêlo <tiago.campelo@liferay.com>
2025-06-19 01:56:04 -0300

    add dynamic bg to pokemon sprite

--- src/app/game-boy/screen/screen.component.ts
+++ src/app/game-boy/screen/screen.component.ts
@@ -1,4 +1,10 @@
-import { Component, Input } from '@angular/core';
+import {
+	Component,
+	Input,
+	OnChanges,
+	OnInit,
+	SimpleChanges,
+} from '@angular/core';
 import { PokemonData } from '../../types';
 import { CommonModule } from '@angular/common';
 
@@ -8,6 +14,38 @@ import { CommonModule } from '@angular/common';
 	templateUrl: './screen.component.html',
 	styleUrl: './screen.component.scss',
 })
-export class ScreenComponent {
+export class ScreenComponent implements OnInit, OnChanges {
 	@Input() pokemonData: PokemonData | null = null;
+
+	backgroundUrl: string = '';
+
+	private backgroundPrefix = '../../../assets/';
+
+	private setRandomBackground(): void {
+		if (this.backgroundImages.length === 0) {
+			return;
+		}
+		const randomIndex = Math.floor(
+			Math.random() * this.backgroundImages.length
+		);
+		this.backgroundUrl = `${this.backgroundPrefix}${this.backgroundImages[randomIndex]}-bg.png`;
+	}
+
+	private backgroundImages: string[] = [
+		'city',
+		'desert',
+		'forest',
+		'savannah',
+		'snow',
+	];
+
+	ngOnInit(): void {
+		this.setRandomBackground();
+	}
+
+	ngOnChanges(changes: SimpleChanges): void {
+		if (changes['pokemonData'] && changes['pokemonData'].currentValue) {
+			this.setRandomBackground();
+		}
+	}
 }
